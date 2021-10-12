edfe0af3289167c91c95d572fde41449c92b13c9
JorgeAndresLucero <jorgeandres31@gmail.com>
2021-10-12 16:14:27 -0700

    Remove of unusable sections for cards and change from 'DDUSOV' to 'OVT' on navbar.

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -71,30 +71,6 @@
                     <h5 class="text-left">DOI: {{result.doi}}</h5>
                     <h5 class="text-left">Fecha de publicación: {{result.pubYear}}</h5>
                   </md-tab>
-
-                  <md-tab id="tab-pages" md-label="Messages" md-icon="chat">
-                    <p>
-                      I think that’s a responsibility that I have, to push
-                      possibilities, to show people, this is the level that things
-                      could be at. I will be the leader of a company that ends up
-                      being worth billions of dollars, because I got the answers.
-                      I understand culture. I am the nucleus. I think that’s a
-                      responsibility that I have, to push possibilities, to show
-                      people, this is the level that things could be at.
-                    </p>
-                  </md-tab>
-
-                  <md-tab id="tab-posts" md-label="Settings" md-icon="build">
-                    <p>
-                      I think that’s a responsibility that I have, to push
-                      possibilities, to show people, this is the level that things
-                      could be at. So when you get something that has the name
-                      Kanye West on it, it’s supposed to be pushing the furthest
-                      possibilities. I will be the leader of a company that ends
-                      up being worth billions of dollars, because I got the
-                      answers. I understand culture. I am the nucleus.
-                    </p>
-                  </md-tab>
                 </md-tabs>
               </template>
             </nav-tabs-card>
